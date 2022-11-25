3894150f81dc7675462f009c722a9c4b41d113da
Simon Scharschinger <simon@scharschinger.de>
2022-11-25 10:20:36 +0100

    .

--- Winget-AutoUpdate-Install.ps1
+++ Winget-AutoUpdate-Install.ps1
@@ -200,6 +200,29 @@ function Install-WinGet {
 
 }
 
+function Get-TenantId {
+    $TenantID = ''
+
+    $DSReg = dsregcmd /status
+
+    $AADJ = $DSReg | Select-String AzureAdJoined
+    $AADJ = ($AADJ.tostring() -split ":")[1].trim()
+    if ($AADJ -eq "YES"){
+        $TenantID = $DSReg | Select-String TenantId | Select-Object -First 1
+        $TenantID = ($TenantID.tostring() -split ":")[1].trim()
+    }
+
+    $WPJ = $DSReg | Select-String WorkplaceJoined | Select-Object -First 1
+    $WPJ = ($WPJ.tostring() -split ":")[1].trim()
+    if ($WPJ -eq 'YES'){
+        $TenantID = $DSReg | Select-String WorkplaceTenantId | Select-Object -First 1
+        $TenantID = ($TenantID.tostring() -split ":")[1].trim()
+    }
+    return $TenantID
+}
+
+
+
 function Install-WingetAutoUpdate {
 
     Write-Host "`nInstalling WAU..." -ForegroundColor Yellow
